22b4bc535f0fa7b7257620345f84d901b69d4818
PFMello <philipmikh@gmail.com>
2023-11-08 16:23:30 -0300

    codigo refatorado e otimizado

--- script.js
+++ script.js
@@ -5,27 +5,26 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++)
-  btnOpenModal[i].addEventListener('click', function () {
-    modal.classList.toggle('hidden');
-    overlay.classList.toggle('hidden');
-  });
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
-btnCloseModal.addEventListener('click', function () {
+const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
-});
+};
+
+for (let i = 0; i < btnOpenModal.length; i++)
+  btnOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
 
 document.addEventListener('keydown', function (event) {
   if (event.key === 'Escape') {
-    modal.classList.add('hidden');
-    overlay.classList.add('hidden');
+    closeModal();
   }
-});
-
-overlay.addEventListener('click', function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
 });
